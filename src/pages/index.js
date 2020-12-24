ecf2e2fd9a658c88404da71a72f001b0c275f2c0
Phillip Tran <ptrandev@protonmail.com>
2020-12-24 01:21:46 -0500

    added avatar to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,43 @@ import { css } from "@emotion/core"
 import { Link, graphql } from "gatsby"
 import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
+import styled from "@emotion/styled"
+
+
+const Avatar = styled.img`
+max-height: 200px;
+border-radius: 50%;
+margin-right: 0;
+margin-bottom: ${rhythm(1)};
+
+@media (min-width: 576px) {
+  margin-bottom: 0;
+  margin-right: ${rhythm(1)};
+}
+`
+
+const Introduction = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+margin-bottom: ${rhythm(1)};
+
+p {
+  margin: 0;
+}
+
+@media (min-width: 576px) {
+  flex-direction: row;
+}
+`
+
+const Greeting = styled.h1`
+text-align: center;
+
+@media (min-width: 576px) {
+  text-align: left;
+}
+`
 
 export default function Home() {
   return (
@@ -11,14 +48,17 @@ export default function Home() {
       description="Personal portfolio of Phillip Tran: a freshman at Boston
       University with an interest in computer science and biology."
     >
-      <h1>
+      <Greeting>
         Hi, I'm Phillip.
-      </h1>
+      </Greeting>
+      <Introduction>
+      <Avatar src="https://res.cloudinary.com/donutdeflector/image/upload/h_300,f_auto,q_auto:/v1608789330/ptran.dev/avatar.jpg"></Avatar>
       <p>
         I'm a freshman at Boston University with an interest in computer science
         and biology. I'm a founding member of Forvava, a startup aiming to
         help people rent/lease storage and parking space.
       </p>
+      </Introduction>
       <p>
         Within the world of software enginneering, I am most interest in crafting
         beautiful, functional web applications and developing robust, adaptable
