9045e0977ad88e1c38279961954bf1f788f111c8
Phillip Tran <ptrandev@protonmail.com>
2023-06-10 16:46:05 -0400

    added new paragraph

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,6 +71,9 @@ export default function Home() {
       <p>
         If I'm not programming, you'll find me working on my music or co-hosting podcasts. Go ahead and check out my media involvement <Link to="/media">here</Link>.
       </p>
+      <p>
+        Outside of these things, I enjoy personal finance, fitness, cooking, and watches. I love staying curious!
+      </p>
     </Layout>
   )
 }
